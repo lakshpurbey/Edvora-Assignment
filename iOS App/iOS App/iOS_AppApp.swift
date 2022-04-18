d30dd3fb4ebdf6a2384d303907e7ee2622cfe644
Your Name <you@example.com>
2022-04-18 07:57:02 +0545

    Initial Setup

---
+++ iOS App/iOS App/iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_AppApp.swift
+//  iOS App
+//
+//  Created by Mac on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
